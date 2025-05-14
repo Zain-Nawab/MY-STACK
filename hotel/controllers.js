ade4b67066687df9e0acae580571f0c77b613d37
Talha Mughal <164769621+Zain-Nawab@users.noreply.github.com>
2025-05-14 14:23:54 +0500

    Update controllers.js

--- hotel/controllers.js
+++ hotel/controllers.js
@@ -10,3 +10,38 @@ php artisan make:controller Admin/RoomController --resource
 php artisan make:controller Admin/BookingController
 php artisan make:controller Admin/UserController
 
+
+class AuthController extends Controller
+{
+    public function register(Request $request) { /* Validate and create user */ }
+    public function login(Request $request) { /* Check credentials and login */ }
+    public function logout(Request $request) { /* Logout */ }
+}
+
+
+class RoomController extends Controller
+{
+    public function index() { /* List available rooms with filters */ }
+    public function show($id) { /* Room details */ }
+}
+
+
+class BookingController extends Controller
+{
+    public function index() { /* List user's bookings */ }
+    public function store(Request $request) { /* Create new booking */ }
+    public function cancel($id) { /* Cancel booking */ }
+}
+
+
+class PaymentController extends Controller
+{
+    public function create($bookingId) { /* Show payment page */ }
+    public function process(Request $request) { /* Stripe/PayPal integration */ }
+}
+
+class ReviewController extends Controller
+{
+    public function store(Request $request) { /* Add review */ }
+}
+

fc2244130458565cb400b260f17b60cefb15ab99
Talha Mughal <164769621+Zain-Nawab@users.noreply.github.com>
2025-05-14 14:22:51 +0500

    Update controllers.js

--- hotel/controllers.js
+++ hotel/controllers.js
@@ -1,1 +1,12 @@
 
+
+php artisan make:controller AuthController
+php artisan make:controller RoomController
+php artisan make:controller BookingController
+php artisan make:controller PaymentController
+php artisan make:controller ReviewController
+php artisan make:controller Admin/DashboardController
+php artisan make:controller Admin/RoomController --resource
+php artisan make:controller Admin/BookingController
+php artisan make:controller Admin/UserController
+
